706dbb3df6f2dded9e94f3d0985a045b00c85ec1
Priansh Shah <shah@priansh.com>
2018-01-04 15:21:10 -0500

    fix authentication, make window bigger

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const electron = require('electron')
 const { app, BrowserWindow, Tray } = require('electron')
 const shortcut = require('electron-localshortcut');
-
+const {username, password, year} = require('./config')
 var hideTab = false
 let tray = null
 var open = false
@@ -9,7 +9,7 @@ var open = false
 var launch = () => {
     open = true
     let win = new BrowserWindow({
-        width: 800, height: 600, icon: __dirname + '/icon.ico',        
+        width: 1160, height: 740, icon: __dirname + '/icon.ico',        
         webPreferences: {
             plugins: true,
             nodeIntegration: false
@@ -27,7 +27,12 @@ var launch = () => {
         () => { win.setFullScreen(!win.isFullScreen()); win.center() })
     shortcut.register(win, 'Ctrl+Y', () => { win.center() })
     shortcut.register(win, 'Ctrl+Shift+I', () => { win.webContents.toggleDevTools() })
-    win.loadURL('http://cate.doc.ic.ac.uk/')
+    let url = `https://${username}:${password}@cate.doc.ic.ac.uk/personal.cgi?keyp=${year}:${username}`
+    win.loadURL(url)
+    app.on('login', function(e, wc, rq, auth, cb) {
+        e.preventDefault();
+        cb(username, password);
+    })
     win.webContents.on('dom-ready', (e, d) => {
         // win.webContents.insertCSS
     })
